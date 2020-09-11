966e26d7813373762633d702e1f5f45a8fe07d0b
Forbes Lindesay <forbes@lindesay.co.uk>
2020-09-11 15:24:03 +0100

    feat: add pick and omit utilities

---
+++ src/types/Object.test.ts
@@ -0,0 +1,25 @@
+import { Object as ObjectType, String } from '..';
+
+test('pick', () => {
+  const CrewMember = ObjectType({
+    name: String,
+    rank: String,
+    home: String,
+  });
+  const PetMember = CrewMember.pick('name', 'home');
+
+  expect(Object.keys(PetMember.fields)).toEqual(['name', 'home']);
+  expect(PetMember.safeParse({ name: 'my name', home: 'my home' })).toMatchInlineSnapshot();
+});
+
+test('omit', () => {
+  const CrewMember = ObjectType({
+    name: String,
+    rank: String,
+    home: String,
+  });
+  const PetMember = CrewMember.omit('rank');
+
+  expect(Object.keys(PetMember.fields)).toEqual(['name', 'home']);
+  expect(PetMember.safeParse({ name: 'my name', home: 'my home' })).toMatchInlineSnapshot();
+});
