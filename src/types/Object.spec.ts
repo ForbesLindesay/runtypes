d612cc5cd200d17dd00b0b2cbe484db5d73a54f4
Forbes Lindesay <forbes@lindesay.co.uk>
2020-09-11 15:36:13 +0100

    test: add test case for super edge case to satisfy coveralls

--- src/types/Object.spec.ts
+++ src/types/Object.spec.ts
@@ -39,3 +39,25 @@ test('omit', () => {
     }
   `);
 });
+
+test('omit arbitrary string does not actually break', () => {
+  const CrewMember = ObjectType({
+    name: String,
+    rank: String,
+    home: String,
+  });
+
+  // The type inference will fail here, but we shouldn't get a runtime error
+  const PetMember = CrewMember.omit('rank', 'foobar' as any);
+
+  expect(Object.keys(PetMember.fields)).toEqual(['name', 'home']);
+  expect(PetMember.safeParse({ name: 'my name', home: 'my home' })).toMatchInlineSnapshot(`
+    Object {
+      "success": true,
+      "value": Object {
+        "home": "my home",
+        "name": "my name",
+      },
+    }
+  `);
+});
